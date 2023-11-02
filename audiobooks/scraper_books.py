39eef001e44e62a0d3da978297bc84c3e986219d
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-11-03 01:57:48 +0200

    del comments

--- audiobooks/scraper_books.py
+++ audiobooks/scraper_books.py
@@ -5,13 +5,9 @@ import time
 web = "https://www.audible.com/search"
 driver = webdriver.Chrome()
 driver.get(web)
-# driver.maximize_window()
-# time.sleep(1)
 
 container = driver.find_element('xpath', '//*[@id="center-3"]/div/div/div/span[2]/ul')
-# product = driver.find_element("xpath", './li')
 products = container.find_elements('xpath','.//li[contains(@class, "productListItem")]')
-# print(products)
 
 title = []
 author = []
